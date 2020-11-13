a18a18b73c3004ab7977fe88c3890f76dfc4ac4c
miathom <73457146+miathom@users.noreply.github.com>
2020-11-13 01:05:51 -0700

    Create 005.py

---
+++ python/pybyexample/005.py
@@ -0,0 +1,7 @@
+num1 = input("Enter number 1: ")
+num2 = input("Enter number 2: ")
+num3 = input("Enter number 3: ")
+
+number = (int(num1) + int(num2)) * int(num3)
+
+print(f'The answer is {number}')
